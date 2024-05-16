2740f7cc4ab2301be4c100286ba097b5213c4d53
Reinaldy Hutapea <reinaldyhutapea@gmail.com>
2024-05-16 14:39:42 +0700

    mvc imple final

--- src/main/java/pbo/f01/App.java
+++ src/main/java/pbo/f01/App.java
@@ -18,6 +18,8 @@ public class App {
         factory = Persistence.createEntityManagerFactory("dormy_pu");
         entityManager = factory.createEntityManager();
 
+        Executor executor = new Executor(entityManager);
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             if (command.equals("---")) {
@@ -27,100 +29,24 @@ public class App {
             String[] tokens = command.split("#");
             switch (tokens[0]) {
                 case "student-add":
-                    studentAdd(tokens);
+                    executor.studentAdd(tokens);
                     break;
                 case "dorm-add":
-                    dormAdd(tokens);
+                    executor.dormAdd(tokens);
                     break;
                 case "assign":
-                    assignStudent(tokens);
+                    executor.assignStudent(tokens);
                     break;
                 case "display-all":
-                    displayAll();
+                    executor.displayAll();
                     break;
             }
         }
 
-        cleanupDatabase();
+        executor.cleanupDatabase();
         scanner.close();
-    }
-
-    private static void studentAdd(String[] tokens) {
-        String s_Id = tokens[1];
-        String s_name = tokens[2];
-        String s_entranceYear = tokens[3];
-        String s_gender = tokens[4];
-
-        // Cek apakah mahasiswa dengan s_Id tersebut sudah ada
-        Student existingStudent = entityManager.find(Student.class, s_Id);
-        if (existingStudent != null) {
-            return;
-        }
-
-        entityManager.getTransaction().begin();
-        Student student = new Student(s_Id, s_name, s_entranceYear, s_gender);
-        entityManager.persist(student);
-        entityManager.getTransaction().commit();
-    }
-
-    private static void dormAdd(String[] tokens) {
-        String d_name = tokens[1];
-        int d_capacity = Integer.parseInt(tokens[2]);
-        String d_gender = tokens[3];
-
-        entityManager.getTransaction().begin();
-        Dorm dorm = new Dorm(d_name, d_capacity, d_gender);
-        entityManager.persist(dorm);
-        entityManager.getTransaction().commit();
-    }
-
-    private static void assignStudent(String[] tokens) {
-        String s_Id = tokens[1];
-        String d_name = tokens[2];
-
-        Student student = entityManager.find(Student.class, s_Id);
-        Dorm dorm = entityManager.find(Dorm.class, d_name);
-
-        if (student == null || dorm == null) {
-            return;
-        }
-
-        // Kondisi untuk memastikan gender student sesuai dengan gender dorm
-        if (!student.getS_gender().equals(dorm.getD_gender())) {
-            return;
-        }
-
-        if (dorm.getStudents().size() >= dorm.getD_capacity()) {
-            return;
-        }
-
-        entityManager.getTransaction().begin();
-        dorm.getStudents().add(student);
-        entityManager.merge(dorm);
-        entityManager.getTransaction().commit();
-    }
-
-    private static void displayAll() {
-        List<Dorm> dorms = entityManager.createQuery("SELECT d FROM Dorm d ORDER BY d.d_name", Dorm.class).getResultList();
-
-        for (Dorm dorm : dorms) {
-            System.out.println(dorm);
-
-            List<Student> students = new ArrayList<>(dorm.getStudents());
-            students.sort(Comparator.comparing(Student::getS_name));
-
-            for (Student student : students) {
-                System.out.println(student);
-            }
-        }
-    }
-
-    private static void cleanupDatabase() {
-        entityManager.getTransaction().begin();
-        entityManager.createQuery("DELETE FROM Student").executeUpdate();
-        entityManager.createQuery("DELETE FROM Dorm").executeUpdate();
-        entityManager.getTransaction().commit();
-        entityManager.close();
         factory.close();
     }
+
+  
 }
---
+++ src/main/java/pbo/f01/model/Executor.java
@@ -0,0 +1,97 @@
+package pbo.f01.model;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import javax.persistence.*;
+/**
+ * 12S22010 - Reinaldy Hutapea
+ * 12S22048 - Ira Wianda Sari Silalahi
+ */
+public class Executor {
+    private EntityManager entityManager;
+
+    public Executor(EntityManager em){
+        this.entityManager = em;
+    }
+
+      public void studentAdd(String[] tokens) {
+        String s_Id = tokens[1];
+        String s_name = tokens[2];
+        String s_entranceYear = tokens[3];
+        String s_gender = tokens[4];
+
+        // Cek apakah mahasiswa dengan s_Id tersebut sudah ada
+        Student existingStudent = entityManager.find(Student.class, s_Id);
+        if (existingStudent != null) {
+            return;
+        }
+
+        entityManager.getTransaction().begin();
+        Student student = new Student(s_Id, s_name, s_entranceYear, s_gender);
+        entityManager.persist(student);
+        entityManager.getTransaction().commit();
+    }
+
+    public void dormAdd(String[] tokens) {
+        String d_name = tokens[1];
+        int d_capacity = Integer.parseInt(tokens[2]);
+        String d_gender = tokens[3];
+
+        entityManager.getTransaction().begin();
+        Dorm dorm = new Dorm(d_name, d_capacity, d_gender);
+        entityManager.persist(dorm);
+        entityManager.getTransaction().commit();
+    }
+
+    public void assignStudent(String[] tokens) {
+        String s_Id = tokens[1];
+        String d_name = tokens[2];
+
+        Student student = entityManager.find(Student.class, s_Id);
+        Dorm dorm = entityManager.find(Dorm.class, d_name);
+
+        if (student == null || dorm == null) {
+            return;
+        }
+
+        // Kondisi untuk memastikan gender student sesuai dengan gender dorm
+        if (!student.getS_gender().equals(dorm.getD_gender())) {
+            return;
+        }
+
+        if (dorm.getStudents().size() >= dorm.getD_capacity()) {
+            return;
+        }
+
+        entityManager.getTransaction().begin();
+        dorm.getStudents().add(student);
+        entityManager.merge(dorm);
+        entityManager.getTransaction().commit();
+    }
+
+    public void displayAll() {
+        List<Dorm> dorms = entityManager.createQuery("SELECT d FROM Dorm d ORDER BY d.d_name", Dorm.class).getResultList();
+
+        for (Dorm dorm : dorms) {
+            System.out.println(dorm);
+
+            List<Student> students = new ArrayList<>(dorm.getStudents());
+            students.sort(Comparator.comparing(Student::getS_name));
+
+            for (Student student : students) {
+                System.out.println(student);
+            }
+        }
+    }
+
+    public void cleanupDatabase() {
+        entityManager.getTransaction().begin();
+        entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        entityManager.createQuery("DELETE FROM Dorm").executeUpdate();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        
+    }
+}
